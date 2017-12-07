193e813c2792727e925a76349797758d1aed3e69
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-07 13:01:03 +0100

    final commit

---
+++ src/Zad06.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Zad06 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int i = 0;
+        boolean condition = true;
+
+        do {
+            System.out.println("Podaj liczbę:");
+            i = scan.nextInt();
+            if (i < 100) {
+                System.out.println("Podana liczba jest za mała");
+            } else if (i > 200) {
+                System.out.println("Podana liczba jest za duża");
+            } else if (i >= 100 && i <= 200) {
+                if (i % 3 == 0) {
+                    System.out.println("Twoja liczba jest ok");
+                    break;
+                } else {
+                    System.out.println("Liczba nie jest podzielna przez 3");
+                }
+            }
+        } while (condition);
+    }
+}
